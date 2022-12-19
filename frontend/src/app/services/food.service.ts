adf10f459a02d2b7621872dc0271f1ee842cfbf4
Kat165 <kasia17wilk@gmail.com>
2022-12-19 15:32:03 +0100

    basic search

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -12,4 +12,8 @@ export class FoodService {
   getAll():Food[]{
     return sample
   }
+
+  getAllFoodBySearchTerm(searchTerm:string){
+    return this.getAll().filter(food => food.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) || food.origins.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
+  }
 }
