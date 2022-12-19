3a6fb318c9ae8f83f4ee2dc553352e00a454fab6
Kat165 <kasia17wilk@gmail.com>
2022-12-19 17:47:32 +0100

    food pagev1

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
 
 @Component({
   selector: 'app-food-page',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./food-page.component.css']
 })
 export class FoodPageComponent {
+  food!:Food
+  constructor(activatedRoute:ActivatedRoute,foodService:FoodService){
+    activatedRoute.params.subscribe((params) => {
+      if(params.id)
+      this.food = foodService.getFoodById(params.id)
+    })
+  }
 
 }
