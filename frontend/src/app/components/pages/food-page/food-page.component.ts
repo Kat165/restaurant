d3017a51b147ca4646c41b458d2827cb1ef0a795
Kat165 <kasia17wilk@gmail.com>
2022-12-20 21:49:49 +0100

    add-del

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -11,17 +11,62 @@ import { Food } from 'src/app/shared/models/Food';
 })
 export class FoodPageComponent {
   food!:Food
+  startStock!:number
   constructor(activatedRoute:ActivatedRoute,foodService:FoodService,
     private cartService:CartService, private router:Router){
     activatedRoute.params.subscribe((params) => {
       if(params.id)
       this.food = foodService.getFoodById(params.id)
+      this.startStock = this.food.inStock
     })
   }
 
   addToCart(){
     this.cartService.addToCart(this.food);
     this.router.navigateByUrl('/cart-page');
+  }
+
+  addQuant(){
+    let p = document.getElementById('infotext')
+    let x = ''
+    let y =0
+    if(this.food.inStock>0){
+      this.food.inStock--
+      this.cartService.addToCart(this.food);
+      if(p!=undefined){
+        if (p.textContent!=null)
+          x = p.textContent
+        y = parseInt(x) + 1;
+        p.innerHTML = `${this.startStock - this.food.inStock}`
+
+      }
+    }
+    this.cartService.addToCart(this.food);
+    this.cartService.changeQuantity(this.food.id,y)
+  }
+
+  delQuant(){
+    let p = document.getElementById('infotext')
+    let x = ''
+    let y =0
+    if(this.food.inStock>0){
+
+      //this.cartService.addToCart(this.food);
+      if(p!=undefined){
+
+        if (p.textContent!=null)
+          x = p.textContent
+        y = parseInt(x) - 1;
+        if(y<0)
+          return
+        this.food.inStock++
+        p.innerHTML = `${this.startStock - this.food.inStock}`
+
+
+      }
+    }
+    //this.cartService.addToCart(this.food);
+    this.cartService.changeQuantity(this.food.id,y)
   }
 
 }
--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -10,7 +11,12 @@ import { Food } from 'src/app/shared/models/Food';
 })
 export class HomeComponent {
   foods:Food[] = []
-  constructor(private foodService:FoodService,activatedRoute:ActivatedRoute){
+  minprice:number = 0
+  maxprice:number = 0
+  prices:number[] = []
+  quants = new Map<string, number>();
+  constructor(private foodService:FoodService,activatedRoute:ActivatedRoute,
+    private cartService:CartService, private router:Router){
     activatedRoute.params.subscribe((params) =>{
       if(params.searchTerm)
       this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm);
@@ -19,5 +25,33 @@ export class HomeComponent {
       else
       this.foods = foodService.getAll()
     })
+
+    for (let index = 0; index < this.foods.length; index++) {
+      if(this.foods[index].inStock > 0)
+        this.prices.push(this.foods[index].price)
+      this.quants.set(this.foods[index].id,0)
+      console.log("x")
+    }
+
+    this.minprice = Math.min.apply(null,this.prices)
+    this.maxprice = Math.max.apply(null,this.prices)
   }
+/*
+  AddClick(food:Food){
+    if(food.inStock>0){
+      food.inStock--
+      this.cartService.addToCart(food);
+      let idd = food.id
+      let q = this.quants.get(food.id)
+      console.log(q)
+      if(!q)
+        q = 0
+      let m = q+1
+      this.quants.set(food.id,m)
+      console.log(this.quants.get(food.id))
+      this.cartService.changeQuantity(food.id, m)
+
+      console.log(this.quants)
+    }
+  }*/
 }
